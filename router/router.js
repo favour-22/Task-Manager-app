308a3e585afea1fbe9db002fc093eb2540ddfbfb
favour-22 <105045421+favour-22@users.noreply.github.com>
2023-11-09 20:40:20 +0100

    FIX:fixed error

--- router/router.js
+++ router/router.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/',(req,res)=>{
-    res.json({message:"calling API"}); 
+router.get('/', (req, res) => {
+    res.json({ message: " default page" });
 });
 
 module.exports = router;
